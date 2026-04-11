b46ea1c60c94801403d10c0d9dc1d82428fe5791
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2026-04-11 19:03:01 +0300

    корректировки редактирования имени пользователя

--- routes/user.py
+++ routes/user.py
@@ -21,16 +21,18 @@ router = APIRouter(prefix="/user", tags=["User"])
 
 @router.post("/redact_name", response_model=SuccessResponse)
 @db_transaction
-def redact_user_name(name: str, db: Session = Depends(get_db)):
+def redact_user_name(name: str, current_user: User = Depends(get_user), db: Session = Depends(get_db)):
     if not name:
         raise empty_user_name_exception
 
-    user_db = db.query(User).filter_by(name=name).first()
-
-    if not user_db:
+    if not current_user:
         raise user_exception
 
-    user_db.name = name
+    current_user.name = name
+
+    return {
+        "success": True,
+    }
 
 
 @router.post("/redact_avatar", response_model=AvatarResponse)
